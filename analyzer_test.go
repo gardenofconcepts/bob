92c650c1f9bb545f7538e5df9cef73eed5e7663c
Dennis Oehme <oehme@gardenofconcepts.com>
2017-01-19 18:52:13 +0100

    Added tests

--- analyzer_test.go
+++ analyzer_test.go
@@ -1,49 +1,15 @@
 package main
 
 import (
-	/*"fmt"
-	"os"
-	"path/filepath"*/
 	"testing"
 )
 
 func TestAnalyzer(t *testing.T) {
 
-	t.Skip("Not implemented yet")
+	expectedHash := "9c90746368d07aa971e4ddd37e7d5c98"
+	hash, _ := Analyzer("assets/analyzer", []string{"**"}, []string{"*.js"})
 
-	/*baseDir := "/var/www"
-	fileDir := "/var/www/src/bundle/werndl"
-	globPath := "../../../web/**"
-
-	globPath = filepath.Join(fileDir, globPath)
-
-	t.Error("gp", globPath)
-
-	globPath, _ = filepath.Rel(baseDir, globPath)
-
-	t.Error("gp", globPath)
-
-	fmt.Print(baseDir)
-
-	pwd, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	if hash != expectedHash {
+		t.Fatalf("Expect hash '%s', instead of %s", expectedHash, hash)
 	}
-
-	glob := "../../../builder/assets/**"
-
-	pwd = filepath.Join(pwd, glob)
-
-	t.Error("Path: ", pwd)
-
-	hash, err := Analyzer("src/../assets/hash", []string{"**"}, []string{})
-
-	if err != nil {
-		t.Error("Error", err)
-	}
-
-	if hash != "er" {
-		t.Error(hash)
-	}*/
 }
